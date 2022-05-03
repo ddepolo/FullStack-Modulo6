9cb5e42422944b21876b489825996bd20ae491e9
ddepolo <diego.de@gmail.com>
2022-05-02 23:45:49 -0300

    Se agrega db datos.sql y se suprime la otra que fue enviada por error.

---
+++ unidad2/datos.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 03-05-2022 a las 02:45:09
+-- Versión del servidor: 8.0.27
+-- Versión de PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `datos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+CREATE TABLE IF NOT EXISTS `empleados` (
+  `id` mediumint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `telefono` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `cp` varchar(10) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `pais` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `tel` mediumint DEFAULT NULL,
+  `dir` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `id_img` varchar(300) COLLATE utf8_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=514 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `empleados`
+--
+
+INSERT INTO `empleados` (`id`, `nombre`, `telefono`, `email`, `cp`, `pais`, `tel`, `dir`, `id_img`) VALUES
+(0, 'Tanisha Mason', '1-722-778-2436', 'diam.pellentesque@yahoo.com', '83640-718', 'United Kingdom', 7, 'Ap #508-9781 Quisque St.', NULL),
+(1, 'Hunter Payne', '1-225-201-7124', 'interdum.curabitur@icloud.couk', '25390', 'Australia', 6, '661-402 Egestas Road', NULL),
+(2, 'Edan Mclean', '(150) 162-7231', 'rutrum.fusce@protonmail.net', '265736', 'Poland', 1, '850-6779 Erat Street', NULL),
+(3, 'Raja Beach', '(573) 824-4985', 'hendrerit.id@aol.net', 'B2P 8P4', 'Belgium', 10, 'P.O. Box 867, 7318 Egestas St.', NULL),
+(4, 'Chester Christensen', '1-507-614-1642', 'neque.nullam@hotmail.org', '163617', 'Pakistan', 4, '6431 Torquent Rd.', NULL);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
